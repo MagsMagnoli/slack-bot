fe8dddc24383e74a8f03c5067d5b681dcb8afb4b
Mags <dev@mags.page>
2020-11-08 09:48:38 -0500

    optimize cloud client

--- src/google-sheets/index.ts
+++ src/google-sheets/index.ts
@@ -1,12 +1,12 @@
 import { google } from 'googleapis';
 
+const auth = new google.auth.GoogleAuth({
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
+
+const api = google.sheets({ version: 'v4' });
+
 export const googleSheets = async () => {
-  const auth = new google.auth.GoogleAuth({
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  });
-
-  const api = google.sheets({ version: 'v4' });
-
   const client = await auth.getClient();
 
   return {
